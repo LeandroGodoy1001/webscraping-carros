3c3f58bf0113be28a2f3e78a9be3c36a59972986
Leandro <leandrogodoy1001@gmail.com>
2022-01-21 19:20:10 -0300

    correção tipo de variavel do nome do veiculo

--- Sites/Movida.py
+++ Sites/Movida.py
@@ -143,7 +143,8 @@ class Movida:
                 car.click()
                 sleep(8)
 
-                dados_carro['Nome'] = self.navegador.find_element(By.XPATH, '//h1[@class="subtitle-car-detail"]').text
+                dados_carro['Nome'] = self.navegador.find_element(By.XPATH, '//p[@class="subtitle-car-detail"]').text
+                print(dados_carro['Nome'])
 
                 # Descendo na página para evitar problemas de não conseguir acessar o objetivo por estar fora da tela ou com algo na frente.
                 self.navegador.execute_script('window.scrollBy(0, 200)')
@@ -207,6 +208,7 @@ class Movida:
             # Voltando para a página inicial.
             self.pagina_inicial()
             i += 1
+            print(self.dataframe)
 
         print(f'Coleta do site {self.url} finalizada')
         self.navegador.close()
